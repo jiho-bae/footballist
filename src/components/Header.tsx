c478859b3025845820c967b1b403bffad7476e22
jiho-bae <jiho@dgu.ac.kr>
2022-12-13 17:38:55 +0900

    Refactor: 헤더 탭에 nav태그 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,26 +8,26 @@ function Header() {
   const pathIdx = indexOfPathname(pathname);
 
   return (
-    <div className="font-mono px-7 py-3 h-24">
-      <ul className="flex justify-between items-center h-full">
-        <div className="cursor-pointer h-full">
-          <li className="text-6xl font-bold ">
-            <Link to={PAGE_PATH.home}>Footballist ⚽️</Link>
-          </li>
-        </div>
-        <div className="flex justify-between items-center text-3xl gap-5 h-full">
-          {PAGES.map((page, idx) => (
-            <li
-              key={[page, idx].join('-')}
-              className={`h-full leading-loose ${idx === pathIdx ? 'border-b-2 border-main-blue' : null}`}
-            >
-              <Link className="inline-block h-full" to={PAGE_PATH[page]}>
-                {PAGE_NAME[page]}
-              </Link>
-            </li>
-          ))}
-        </div>
-      </ul>
+    <div className="font-mono px-7 py-3 h-24 flex justify-between">
+      <div className="cursor-pointer h-full text-6xl font-bold">
+        <Link to={PAGE_PATH.home}>Footballist ⚽️</Link>
+      </div>
+      <nav>
+        <ul className="flex justify-between items-center h-full">
+          <div className="flex justify-between items-center text-3xl gap-5 h-full">
+            {PAGES.map((page, idx) => (
+              <li
+                key={[page, idx].join('-')}
+                className={`h-full leading-loose ${idx === pathIdx ? 'border-b-2 border-main-blue' : null}`}
+              >
+                <Link className="inline-block h-full" to={PAGE_PATH[page]}>
+                  {PAGE_NAME[page]}
+                </Link>
+              </li>
+            ))}
+          </div>
+        </ul>
+      </nav>
     </div>
   );
 }
