da3777881eec163d090ce926b5a1a132eb6566de
jiho-bae <jiho@dgu.ac.kr>
2022-12-20 21:16:23 +0900

    Feat: Home에서 recoilValue로 Open api의 하이라이트 비디오 가져오도록 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-import getVideos from '../api/getVideos';
 import HighlightFilter from '../components/Filter';
 import HighlightGrid from '../components/HighlightGrid';
-import { getPrevDatesFromToday, PrevDatesVideosType, getPrevDatesVideos } from '../libs/utilFns';
+import { videoState } from '../recoils/video';
+import { getPrevDatesFromToday, getPrevDatesVideos } from '../libs/utilFns';
+import { useRecoilValue } from 'recoil';
 
 export interface VideoType {
   competition: CompetitionType;
@@ -32,34 +33,20 @@ interface VideoTagType {
 }
 
 function Home() {
-  const [isLoading, setIsLoading] = useState(true);
+  const videos = useRecoilValue(videoState);
   const [selectedDate, setSelectedDate] = useState(new Date().toLocaleDateString());
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [prevDates, setPrevDates] = useState(getPrevDatesFromToday(7));
-  const [displayVideos, setDisplayVideos] = useState<PrevDatesVideosType>({});
+  const displayVideos = getPrevDatesVideos(videos, prevDates)[selectedDate];
 
   function onChangeSelect(e: React.ChangeEvent<HTMLSelectElement>) {
     setSelectedDate(e.target.value);
   }
 
-  useEffect(() => {
-    (async function () {
-      const videos = await getVideos('https://www.scorebat.com/video-api/v1/');
-      setIsLoading(false);
-      setDisplayVideos(getPrevDatesVideos(videos, prevDates));
-    })();
-  }, [prevDates]);
-
   return (
-    <div className="App">
-      {isLoading ? (
-        <span>Loading Videos...</span>
-      ) : (
-        <div className="px-4 py-0">
-          <HighlightFilter prevDates={prevDates} selectedDate={selectedDate} onChange={onChangeSelect} />
-          <HighlightGrid displayVideos={displayVideos[selectedDate]} />
-        </div>
-      )}
+    <div className="px-4 py-0">
+      <HighlightFilter prevDates={prevDates} selectedDate={selectedDate} onChange={onChangeSelect} />
+      <HighlightGrid displayVideos={displayVideos} />
     </div>
   );
 }
