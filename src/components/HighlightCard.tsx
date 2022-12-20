ccbe7e6bd548f338fbd780dfb0f1df2ce819f7d1
jiho-bae <jiho@dgu.ac.kr>
2022-12-20 19:01:25 +0900

    Feat: 하이라이트 이미지 10번째 이상부터 lazy load 설정

--- src/components/HighlightCard.tsx
+++ src/components/HighlightCard.tsx
@@ -3,18 +3,18 @@ import { VideoType } from '../pages/Home';
 
 import { dateToLocaleString } from '../libs/utilFns';
 
-function HighlightCard({ video, idx }: { video: VideoType; idx: number }) {
+function HighlightCard({ video, idx, isLazy }: { video: VideoType; idx: number; isLazy: boolean }) {
   const { thumbnail, side1, side2, date } = video;
 
   return (
     <Link to={`/highlights/${idx}`} state={{ video }}>
-      <section className="w-full cursor-pointer">
-        <div
+      <section className="w-30 h-90 cursor-pointer">
+        <img
           className="hover:opacity-70 bg-zinc-300 bg-center bg-cover w-30 h-80"
-          style={{
-            backgroundImage: `url(${thumbnail})`,
-          }}
-        ></div>
+          src={thumbnail}
+          alt="highlight video"
+          loading={isLazy ? 'lazy' : undefined}
+        />
         <div className="flex flex-col">
           <span className="color-black text-2xl">
             {side1.name} vs {side2.name}
