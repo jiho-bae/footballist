adf0d11f8b7418ffab7277692b3950f7b5d6a5ef
jiho-bae <jiho@dgu.ac.kr>
2022-12-21 18:25:14 +0900

    Style: 리그순위테이블 스타일 부여

--- src/pages/League.tsx
+++ src/pages/League.tsx
@@ -6,34 +6,38 @@ function League() {
 
   return (
     <div>
-      <section className="text-2xl">
-        <header>리그 순위</header>
-        <ul>
-          <li className="flex gap-3">
-            {tableHeaders.map((header) => (
-              <div key={header}>{header}</div>
-            ))}
-          </li>
+      <section className="text-2xl w-2/5 h-29r overflow-auto">
+        <ul className="text-center">
+          <li className="flex gap-3 border bg-title-gray p-1">
+            {tableHeaders.map((header) => {
+              const cellWidth = header === '팀명' ? 'team' : header === '득실차' ? 'diff' : 'rest';
 
+              return (
+                <div key={header} className={`w-division-${cellWidth}`}>
+                  {header}
+                </div>
+              );
+            })}
+          </li>
           {total.table.map((row) => {
             const { position, team, playedGames, won, draw, lost, points, goalsFor, goalsAgainst, goalDifference } =
               row;
 
             return (
-              <li className="flex gap-3">
-                <div>{position}</div>
-                <div className="flex">
-                  <img src={team.crest} alt="team logo" className="w-4 h-4"></img>
-                  {team.name}
+              <li key={`${position}${team}`} className="flex gap-3 border-x border-b p-1">
+                <div className="w-division-rest">{position}</div>
+                <div className="flex items-center text-2xl w-division-team">
+                  <img src={team.crest} alt="team logo" className="w-6 h-6 ml-4 mr-4"></img>
+                  {team.shortName}
                 </div>
-                <div>{playedGames}</div>
-                <div>{points}</div>
-                <div>{won}</div>
-                <div>{draw}</div>
-                <div>{lost}</div>
-                <div>{goalsFor}</div>
-                <div>{goalsAgainst}</div>
-                <div>{goalDifference}</div>
+                <div className="w-division-rest">{playedGames}</div>
+                <div className="w-division-rest">{points}</div>
+                <div className="w-division-rest">{won}</div>
+                <div className="w-division-rest">{draw}</div>
+                <div className="w-division-rest">{lost}</div>
+                <div className="w-division-rest">{goalsFor}</div>
+                <div className="w-division-rest">{goalsAgainst}</div>
+                <div className="w-division-diff">{goalDifference}</div>
               </li>
             );
           })}
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,19 @@ module.exports = {
         'main-blue': 'cornflowerblue',
         'main-red': 'tomato',
         'main-brown': '#854d0e',
+        'title-gray': 'rgb(209 213 219)',
       },
       maxWidth: {
         '4/5': '80%',
       },
+      width: {
+        'division-team': '30%',
+        'division-diff': '14%',
+        'division-rest': '7%',
+      },
       height: {
         '80vh': '80vh',
+        '29r': '29rem',
       },
     },
   },
