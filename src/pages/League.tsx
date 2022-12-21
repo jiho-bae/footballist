f2fb46ebc0ce49b6d07d1c923b9fa88e8f7231fc
jiho-bae <jiho@dgu.ac.kr>
2022-12-21 18:50:53 +0900

    Feat: 리그페이지 최상단에 리그이름 및 정보 추가

--- src/pages/League.tsx
+++ src/pages/League.tsx
@@ -2,10 +2,27 @@ import division from '../dummy/division.json';
 
 function League() {
   const tableHeaders = ['순위', '팀명', '경기', '승점', '승', '무', '패', '득', '실', '득실차'];
-  const [total, home, away] = division.standings;
+  const {
+    filters,
+    competition,
+    area,
+    standings: [total, home, away],
+  } = division;
 
   return (
     <div>
+      <header className="flex items-center gap-5">
+        <img src={competition.emblem} alt="league logo" className="w-40 h-40"></img>
+        <div>
+          <h2 className="text-3xl font-medium">
+            {filters.season} {competition.name}
+          </h2>
+          <div className="flex items-center gap-3">
+            <img src={area.flag} alt="national flag" className="w-8 h-8"></img>
+            <span className="text-2xl">{area.name}</span>
+          </div>
+        </div>
+      </header>
       <section className="text-2xl w-2/5 h-29r">
         <div className="flex gap-3 border bg-title-gray p-1 text-center pr-6">
           {tableHeaders.map((header) => {
